156dbe81e0fed0a3ce1b9befd46bb73a2c54750b
Joel Hooks <joelhooks@gmail.com>
2018-09-27 21:35:18 -0700

    add injectGlobals for demo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,36 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { injectGlobal } from 'emotion'
 
 import Layout from '../components/layout'
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+injectGlobal`
+  html, body {
+    font-family: -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      "Roboto",
+      "Roboto Light",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      "Fira Sans",
+      "Droid Sans",
+      "Helvetica Neue",
+      sans-serif,
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol";
+  }
+`
 
 const IndexPage = () => (
   <Layout>
