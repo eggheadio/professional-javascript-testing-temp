21cf1ee72f7930a9f03eb1a58e95b890dcff4d21
Joel Hooks <joelhooks@gmail.com>
2018-09-29 18:16:29 -0700

    don't close auth window

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -37,10 +37,11 @@ export default class Auth {
     if (typeof window !== 'undefined') {
       this.eggheadAuth.token.getToken(location).then(user => {
         this.setSession(user).then(() => {
-          window.onunload = () => {
-            window.opener.location.reload()
-          }
-          window.close()
+          // window.onunload = () => {
+          //   window.opener.location.reload()
+          // }
+          // window.close()
+          window.opener.location.reload()
         })
       })
     }
