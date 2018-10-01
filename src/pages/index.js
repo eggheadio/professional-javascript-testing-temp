6df0b36a864235abe98b902448ee557b420d05f9
DrShpongle <evgeniy.nagalskiy@gmail.com>
2018-10-01 18:55:20 +0300

    PlanTier: wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,6 +63,180 @@ class IndexPage extends React.Component {
             })}
           >
             <Container>123</Container>
+            <section
+              className={css({
+                width: '100%',
+                background: '#ffffff',
+                padding: '4rem 0'
+              })}
+            >
+              <Container maxWidth={768}>
+                <div
+                  className={css({
+                    width: '100%'
+                  })}
+                >
+                  <div
+                    className={css({
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center'
+                    })}
+                  >
+                    <h3
+                      className={css({
+                        fontSize: '34px',
+                        fontWeight: 500,
+                        lineHeight: 1.25,
+                        color: 'black'
+                      })}
+                    >
+                      Start testing like a pro
+                    </h3>
+                    <h6
+                      className={css({
+                        fontSize: '16px',
+                        fontWeight: 400,
+                        lineHeight: 1.25,
+                        color: 'rgba(0, 0, 0, 0.6)',
+                        marginTop: '10px'
+                      })}
+                    >
+                      Buy once. Forever yours.
+                    </h6>
+                    <div
+                      className={css({
+                        marginTop: '4rem',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        width: '100%'
+                      })}
+                    >
+                      <div
+                        className={css({
+                          width: '50%',
+                          flexShrink: 0,
+                          paddingRight: '20px'
+                        })}
+                      >
+                        <div
+                          className={css({
+                            border: '1px solid #e4e4e4'
+                          })}
+                        >
+                          <div
+                            className={css({
+                              padding: '60px 20px',
+                              display: 'flex',
+                              flexDirection: 'column',
+                              alignItems: 'center',
+                              borderBottom: '1px solid #e4e4e4'
+                            })}
+                          >
+                            <h4
+                              className={css({
+                                fontSize: '20px',
+                                lineHeight: 1.25,
+                                fontWeight: 400,
+                                color: 'black'
+                              })}
+                            >
+                              Plan 1
+                            </h4>
+                            <div
+                              className={css({
+                                marginTop: '24px',
+                                display: 'inline-block',
+                                fontSize: '22px',
+                                lineHeight: 1,
+                                color: 'black',
+                                'span': {
+                                  verticalAlign: 'middle',
+                                }
+                              })}
+                            >
+                              <span
+                                className={css({
+                                  fontSize: '16px',
+                                  color: 'rgba(0, 0, 0, 0.5)',
+                                  textTransform: 'uppercase'
+                                })}
+                              >
+                                us
+                              </span>
+                              <span
+                                className={css({
+                                  margin: '0 4px'
+                                })}
+                              >
+                                $
+                              </span>
+                              <span
+                                className={css({
+                                  fontSize: '56px',
+                                  fontWeight: 300
+                                })}
+                              >
+                                149
+                              </span>
+                              <span
+                                className={css({
+                                  marginLeft: '4px',
+                                  fontWeight: 300
+                                })}
+                              >
+                                .00
+                              </span>
+                            </div>
+                            <div className={css({
+                              fontSize: '16px',
+                              color: 'rgba(0, 0, 0, 0.6)',
+                              marginTop: '16px',
+                            })}>
+                              Lorem ipsum dolor sit amet.
+                            </div>
+                          </div>
+                          <div
+                            className={css({
+                              padding: '60px 20px',
+                              display: 'flex',
+                              justifyContent: 'center'
+                            })}
+                          >
+                            <ul className={css({
+                              listStyle: 'none',
+                            })}>
+                              <li className={css({
+                                ':before': {
+                                  content: '"\\2714"',
+                                },
+                              })}>
+
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      <div
+                        className={css({
+                          width: '50%',
+                          flexShrink: 0,
+                          paddingLeft: '20px'
+                        })}
+                      >
+                        <div
+                          className={css({
+                            border: '1px solid #e4e4e4'
+                          })}
+                        >
+                          123
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Container>
+            </section>
           </main>
         </Layout>
       </UserContext.Provider>
