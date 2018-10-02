cdea70215fd85c75c69f5041f6cc80243092f885
DrShpongle <evgeniy.nagalskiy@gmail.com>
2018-10-02 18:57:33 +0300

    add initial color palette

---
+++ src/lib/colorValues.js
@@ -0,0 +1,11 @@
+//TODO: use common variables and not ANOTHER list of vars for this
+const colorValues = {
+  black: '#000',
+  white: '#fff',
+  transparent: 'transparent',
+  'yellow-lighten-15': '#FFD473',
+  'gray-darken-20': '#32323B',
+  'base-orange': '#FF8B66',
+}
+
+export default colorValues
