1913ce76d302e392e4892fdd312c5edbe742ec06
DrShpongle <evgeniy.nagalskiy@gmail.com>
2018-10-02 18:08:23 +0300

    remove unnecessary braces

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,62 +8,60 @@ import { Container } from './markupHelpers'
 
 const auth = new Auth()
 
-const Header = ({ siteTitle, darkHeader }) => {
-  return (
-    <UserContext.Consumer>
-      {({ authenticated }) => {
-        return (
-          <nav
-              className={css({
-                background: darkHeader ? '#32323b' : 'none',
-                width: '100%',
-                padding: '2rem 0'
-              })}
-            >
-              <Container>
-                <div
+const Header = ({ siteTitle, darkHeader }) => (
+  <UserContext.Consumer>
+    {({ authenticated }) => {
+      return (
+        <nav
+            className={css({
+              background: darkHeader ? '#32323b' : 'none',
+              width: '100%',
+              padding: '2rem 0'
+            })}
+          >
+            <Container>
+              <div
+                className={css({
+                  width: '100%',
+                  display: 'flex'
+                })}
+              >
+                <Link
+                  to="/"
                   className={css({
-                    width: '100%',
-                    display: 'flex'
+                    textDecoration: 'none'
                   })}
                 >
-                  <Link
-                    to="/"
-                    className={css({
-                      textDecoration: 'none'
-                    })}
-                  >
-                    Professional Javascript Testing
-                    <br />
-                    <i>with Kent C. Dodds</i>
-                  </Link>
-                  <div
-                    className={css({
-                      flexGrow: 1,
-                      display: 'flex',
-                      justifyContent: 'flex-end'
-                    })}
-                  >
-                    {authenticated ? (
-                      <button
-                        onClick={() => {
-                          auth.logout()
-                          window.location.reload()
-                        }}
-                      >
-                        log out
-                      </button>
-                    ) : (
-                      <button onClick={auth.login}>log in with egghead</button>
-                    )}
-                  </div>
+                  Professional Javascript Testing
+                  <br />
+                  <i>with Kent C. Dodds</i>
+                </Link>
+                <div
+                  className={css({
+                    flexGrow: 1,
+                    display: 'flex',
+                    justifyContent: 'flex-end'
+                  })}
+                >
+                  {authenticated ? (
+                    <button
+                      onClick={() => {
+                        auth.logout()
+                        window.location.reload()
+                      }}
+                    >
+                      log out
+                    </button>
+                  ) : (
+                    <button onClick={auth.login}>log in with egghead</button>
+                  )}
                 </div>
-              </Container>
-            </nav>
-        )
-      }}
-    </UserContext.Consumer>
-  )
-}
+              </div>
+            </Container>
+          </nav>
+      )
+    }}
+  </UserContext.Consumer>
+)
 
 export default Header
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,39 +5,37 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './Header'
 
-const Layout = ({ children, darkHeader }) => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
+const Layout = ({ children, darkHeader }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
           }
         }
-      `}
-      render={data => (
-        <>
-          <Helmet
-            title={data.site.siteMetadata.title}
-            meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' }
-            ]}
-          >
-            <html lang="en" />
-          </Helmet>
-          <Header
-            siteTitle={data.site.siteMetadata.title}
-            darkHeader={darkHeader}
-          />
-          {children}
-        </>
-      )}
-    />
-  )
-}
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' }
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <Header
+          siteTitle={data.site.siteMetadata.title}
+          darkHeader={darkHeader}
+        />
+        {children}
+      </>
+    )}
+  />
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
